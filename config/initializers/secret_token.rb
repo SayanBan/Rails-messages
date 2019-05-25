3994e14da10d0136734f3b6996f0d346243ac717
SayanBan <45221397+SayanBan@users.noreply.github.com>
2019-05-25 10:43:08 +0530

    Create secret_token.rb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MessengerApp::Application.config.secret_token = '16b13abadd2996ab77705dc0366fbcb7bb67108edaad29ae868efccd845ef93cba942de824b6ee22d53a12941396ebc470c2ef2af150f33c4933e835165b1902'
